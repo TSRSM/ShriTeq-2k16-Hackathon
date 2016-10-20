2b5b9e1680da53e4d7f28baebaed97d764078f40
Dhruv Yadav <dhruv1103@gmail.com>
2016-10-20 14:38:26 +0530

    first commit

---
+++ Desktop/Traverse/app/src/main/java/in/dhruvyadav/traverse/RegisterLogin.java
@@ -0,0 +1,38 @@
+package in.dhruvyadav.traverse;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class RegisterLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_login);
+    }
+    public void Clicks(View v)
+    {
+        switch (v.getId())
+        {
+            case R.id.gotologinbutton:
+                Intent login = new Intent(RegisterLogin.this, Login.class);
+                login.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(login);
+                break;
+            case R.id.gotoregisterbutton:
+                Intent register = new Intent(RegisterLogin.this, Register.class);
+                register.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(register);
+                break;
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
---
+++ Desktop/Traverse/app/src/main/java/in/dhruvyadav/traverse/Splash.java
@@ -0,0 +1,36 @@
+package in.dhruvyadav.traverse;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent i = new Intent(Splash.this, RegisterLogin.class);
+                    i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(i);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
